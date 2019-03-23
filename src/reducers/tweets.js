6faab98b5f1fa8dec2d6c2de1a553099bad82feb
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-23 07:40:57 -0700

    Set up adding a new tweet reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from "../actions/tweets";
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from "../actions/tweets";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -17,6 +17,23 @@ export default function users(state = {}, action) {
               ? state[action.id].likes.filter(uid => uid !== action.authedUser)
               : state[action.id].likes.concat([action.authedUser])
         }
+      };
+    case ADD_TWEET:
+      const { tweets } = action;
+      let replyingTo = {};
+      if (tweet.replyingTo !== null) {
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyingTo].replies.concat(tweet.id)
+          }
+        };
+      }
+      //when we add a new tweet we need to add it to the array of our tweets slice of the state. which is an oblect (...state) if the new tweet is a reply to the other tweet then we are going to grab it and add it onto the replies array and spread it onto the state tweets array
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet,
+        ...replyingTo
       };
     default:
       return state;
