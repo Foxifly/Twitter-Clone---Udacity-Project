e120851d06387e2be48eb58e08c081f02a094ef3
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-23 07:18:16 -0700

    Added add new tweet functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import handleInitialData from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
 
 class App extends Component {
   componentDidMount() {
@@ -10,7 +11,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div>{this.props.loading === true ? <LoadingBar /> : <Dashboard />}</div>
+      <div>{this.props.loading === true ? <LoadingBar /> : <NewTweet />}</div>
     );
   }
 }
---
+++ src/components/NewTweet.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class NewTweet extends Component {
+  state = {
+    text: ""
+  };
+  handleChange = e => {
+    const text = e.target.value;
+
+    this.setState(() => ({
+      text
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+  render() {
+    const { text } = this.state;
+    const tweetLeft = 280 - text.length;
+    return (
+      <div>
+        <h3 className="center">Compose New Tweet</h3>
+
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+          <button className="btn" type="submit" disabled={text === ""}>
+            Submit
+          </button>
+          //redirect home
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewTweet;
